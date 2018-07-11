f3681863d65252b9f590d5a4d74f53e0ad3ef3d0
Ivan Kozhokar <dmb14suka@gmail.com>
2018-07-10 18:27:48 -0600

    create function that refreash grid every time user push submit button

--- app.js
+++ app.js
@@ -3,4 +3,10 @@
 	const width = document.querySelector('#inputWidth');
 	const height = document.querySelector('#inputHeight');
 	const canvas = document.querySelector('#pixelCanvas');
+
+	//this function is needed to refresh grid every time user push submit bottom
+	function getRidOfGrid(){
+		canvas.innerHTML = '';
+	}
+
 	
