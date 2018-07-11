08d622a70d167e6d8517df3821cb1ba4ad40130e
Ivan Kozhokar <dmb14suka@gmail.com>
2018-07-10 18:23:56 -0600

    create JS file and stored node elements(Id's), which i need, to variables in DOM

---
+++ app.js
@@ -0,0 +1,6 @@
+	const sizePicker = document.querySelector('#sizePicker');
+	const color = document.querySelector('#colorPicker');
+	const width = document.querySelector('#inputWidth');
+	const height = document.querySelector('#inputHeight');
+	const canvas = document.querySelector('#pixelCanvas');
+	
